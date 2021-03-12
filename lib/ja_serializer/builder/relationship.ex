981b9ffd180218f612163cb47116a81cdd0ec5a0
Peter Brown <pete@lette.us>
2021-03-11 21:11:17 -0500

    Fixes broken links with has_one relationships
    This fixes an issue where we were serializing broken links for null
    has_one relationships if the has_one relationship in the serializer
    specified links.

--- lib/ja_serializer/builder/relationship.ex
+++ lib/ja_serializer/builder/relationship.ex
@@ -56,11 +56,16 @@ defmodule JaSerializer.Builder.Relationship do
     do: do_filter(relationships, Utils.safe_atom_list(fields))
 
   defp empty?(%__MODULE__{data: nil, links: nil, meta: nil}), do: true
+  # defp empty?(%__MODULE__{data: :empty_relationship}), do: true
   defp empty?(%__MODULE__{} = _relationship), do: false
 
   defp add_links(relation, definition, context) do
     definition.links
     |> Enum.map(fn {key, path} -> Link.build(context, key, path) end)
+    |> Enum.reject(fn
+      %{href: nil} -> true
+      _ -> false
+    end)
     |> case do
       [] -> relation
       links -> Map.put(relation, :links, links)
